c8b801bc3f88e4c652c5426346939d3f3f3755d3
mmkclement <maungmaungkha18290@gmail.com>
2023-04-05 10:05:21 +0200

    bind and css

--- src/components/Todos/todos.jsx
+++ src/components/Todos/todos.jsx
@@ -2,26 +2,38 @@ import React, { Component } from "react";
 import TodoItem from "../TodoItem/Todoitem";
 
 class Todos extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [
-        {
-          id: 1,
-          title: "play more",
-        },
-        {
-          id: 2,
-          title: "more ez",
-        },
-      ],
-    };
-  }
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "play more",
+      },
+      {
+        id: 2,
+        title: "more ez",
+      },
+    ],
+    todoTitle: "", // this is going to assign value={this.state.todoTitle }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      todoTitle: e.target.value,
+    });
+  };
 
   render = () => {
     return (
       <div>
         <h1>{this.props.title}</h1>
+        <input
+          style={{ marginLeft: "4rem" }}
+          type="text"
+          name="title"
+          value={this.state.todoTitle}
+          onChange={this.handleChange}
+        />
+        <button>Save</button>
         <ul>
           {this.state.todos.map((todo) => (
             <TodoItem title={todo.title} />
